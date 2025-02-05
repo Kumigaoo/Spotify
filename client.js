cc1bf1c2250795ab4788d1451918b4cec509c152
KumiGaooITICBCN <2023_moises.diez@iticbcn.cat>
2025-02-06 00:29:57 +0100

    todo

---
+++ client.js
@@ -0,0 +1,2 @@
+export const clientId = "add2a5b9e8eb439191e747f49d83bf1c";
+export const clientSecret = "f9954d8edfc84d85a72ff27321311606";
---
+++ main.js
@@ -0,0 +1,297 @@
+import { clientId, clientSecret } from "./client.js";
+
+let tokenAccess = null;
+let searchMessage = "<div class='search-message'>BUSCA UNA CANCIÓN</div>";
+let numTracks = 12;
+
+/**** LISTENERS ****/
+
+// Botón de búsqueda
+searchButton.addEventListener("click", function () {
+  if (searchInput.value === "") {
+    alert("Debes introducir una canción para poder buscar");
+  } else if (searchInput.value.length <= 2) {
+    alert("Debes introducir más de 2 letras para poder buscar");
+  } else if (tokenAccess) {
+    document.getElementById("tracks-container").innerHTML = "";
+    numTracks = 12;
+    search(0);
+  }
+});
+
+// Al presionar Enter
+searchInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    if (searchInput.value === "") {
+      alert("Debes introducir una canción para poder buscar");
+    } else if (searchInput.value.length <= 2) {
+      alert("Debes introducir más de 2 letras para poder buscar");
+    } else if (tokenAccess) {
+      document.getElementById("tracks-container").innerHTML = "";
+      numTracks = 12;
+      search(0);
+    }
+  }
+});
+
+// Botón de borrar
+deleteButton.addEventListener("click", function () {
+  if (tokenAccess && searchInput) {
+    clear();
+  }
+});
+
+// Botón de playlist
+playlistButton.addEventListener("click", function () {
+  autoritzar();
+});
+
+/**** CONEXIÓN API ****/
+function getSpotifyAccessToken(clientId, clientSecret) {
+  const url = "https://accounts.spotify.com/api/token";
+  const credentials = btoa(`${clientId}:${clientSecret}`);
+
+  const header = {
+    Authorization: `Basic ${credentials}`,
+    "Content-Type": "application/x-www-form-urlencoded",
+  };
+
+  fetch(url, {
+    method: "POST",
+    headers: header,
+    body: "grant_type=client_credentials",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      tokenAccess = data.access_token;
+      document.getElementById("searchButton").disabled = false;
+      document.getElementById("playlistButton").disabled = false;
+    })
+    .catch((error) => {
+      console.error("Error al obtener el token:", error);
+    });
+}
+
+/**** BÚSQUEDA ****/
+function search(offset) {
+  const query = document.getElementById("searchInput").value;
+  document.getElementById("deleteButton").disabled = false;
+
+  let searchUrl = `https://api.spotify.com/v1/search?q=${encodeURIComponent(
+    query
+  )}&type=track&limit=12`;
+
+  searchSpotifyTracks(searchUrl, tokenAccess);
+}
+
+function searchSpotifyTracks(searchUrl, accessToken) {
+  fetch(searchUrl, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      renderTracks(data);
+    })
+    .catch((error) => {
+      console.error("Error al buscar canciones:", error);
+    });
+}
+
+/**** RENDERIZAR CANCIONES ****/
+function renderTracks(data) {
+  let tracks = data.tracks.items;
+  let info = data.tracks;
+  const tracksContainer = document.getElementById("tracks-container");
+
+  for (let i = 0; i < tracks.length; i++) {
+    let addButtonText = "+ Añadir";
+    let savedSongs = JSON.parse(localStorage.getItem("savedSongs")) || [];
+
+    if (savedSongs.includes(tracks[i].id)) {
+      addButtonText = "En la lista";
+    }
+
+    const track = document.createElement("div");
+    track.classList.add("track");
+    track.innerHTML = `<img class="track__image" src="${tracks[i].album.images[0].url}" alt="Album cover"/>
+                            <h1>${tracks[i].name}</h1>
+                            <h3>Álbum:</h3>
+                            <h2>${tracks[i].album.name}</h2>
+                            <h3>Artista:</h3>
+                            <h2>${tracks[i].artists[0].name}</h2>
+                            <div class="addButton" id="addButton">
+                                ${addButtonText}
+                            </div>`;
+    tracksContainer.appendChild(track);
+
+    track.addEventListener("click", function () {
+      tracksButton(tracks[i].artists[0].id);
+    });
+
+    const addButton = track.querySelector(`#addButton`);
+    addButton.addEventListener("click", function (event) {
+      event.stopPropagation();
+
+      const buttonText = track.querySelector(".addButton");
+
+      if (savedSongs.includes(tracks[i].id)) {
+        savedSongs = savedSongs.filter((songId) => songId !== tracks[i].id);
+        localStorage.setItem("savedSongs", JSON.stringify(savedSongs));
+        buttonText.innerHTML = "+ Añadir";
+      } else {
+        savedSongs.push(tracks[i].id);
+        localStorage.setItem("savedSongs", JSON.stringify(savedSongs));
+        buttonText.innerHTML = "En la lista";
+
+        document.getElementById("addedMessage").style.opacity = "1";
+        setTimeout(function () {
+          document.getElementById("addedMessage").style.opacity = "0";
+        }, 2500);
+      }
+
+      console.log(savedSongs);
+    });
+  }
+
+  const loadSongs = document.createElement("button");
+  loadSongs.classList.add("loadMoreSong");
+  loadSongs.innerHTML = ` <h4>Cargar más canciones</h3>
+                            <p>${info.offset + info.limit} de ${
+    info.total
+  }</p>`;
+  tracksContainer.appendChild(loadSongs);
+
+  loadSongs.addEventListener("click", function () {
+    searchSpotifyTracks(info.next, tokenAccess);
+    loadSongs.remove();
+  });
+}
+
+/**** BÚSQUEDA DE ARTISTA ****/
+function tracksButton(artist) {
+  const artistUrl = `https://api.spotify.com/v1/artists/${artist}`;
+  artist = null;
+
+  const header = {
+    Authorization: `Bearer ${tokenAccess}`,
+  };
+
+  fetch(artistUrl, {
+    method: "GET",
+    headers: header,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      searchArtistBestSongs(data);
+    })
+    .catch((error) => {
+      console.error("Error al mostrar el artista:", error);
+    });
+}
+
+/**** MEJORES CANCIONES DEL ARTISTA ****/
+function searchArtistBestSongs(artist) {
+  const artistId = artist.id;
+  const artistUrl = `https://api.spotify.com/v1/artists/${artistId}/top-tracks?market=ES`;
+
+  const header = {
+    Authorization: `Bearer ${tokenAccess}`,
+  };
+
+  fetch(artistUrl, {
+    method: "GET",
+    headers: header,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      renderArtist(artist, data);
+    })
+    .catch((error) => {
+      console.error("Error al mostrar el artista:", error);
+    });
+}
+
+/**** RENDERIZAR ARTISTA ****/
+function renderArtist(artist, bestSongs) {
+  const artistContainer = document.getElementById("artist-container");
+  artistContainer.innerHTML = "";
+
+  const artistData = document.createElement("div");
+  artistData.classList.add("artist");
+  artistData.innerHTML = `<img class="artist__image" src="${
+    artist.images[0].url
+  }" alt="Artista"/>
+                            <h1>${artist.name}</h1>
+                            <h3>Popularidad:</h3>
+                            <h2>${artist.popularity}%</h2>
+                            <h3>Géneros:</h3>
+                            <h2>${artist.genres.join(", ")}</h2>
+                            <h3>Seguidores:</h3>
+                            <h2>${artist.followers.total}</h2>
+                            <div class="artistBestSongs">
+                                <h3>Las más escuchadas:</h3>
+                                <div class="songWrapper">
+                                    <h3>1.</h3>
+                                    <h2> ${bestSongs.tracks[0].name}</h2>
+                                </div>
+                                <div class="songWrapper">
+                                    <h3>2.</h3>
+                                    <h2> ${bestSongs.tracks[1].name}</h2>
+                                </div>
+                                <div class="songWrapper">
+                                    <h3>3.</h3>
+                                    <h2> ${bestSongs.tracks[2].name}</h2>
+                                </div>
+                            </div>`;
+  artistContainer.appendChild(artistData);
+}
+
+/**** LIMPIAR INPUT ****/
+function clear() {
+  const tracksContainer = document.getElementById("tracks-container");
+  tracksContainer.innerHTML = searchMessage;
+
+  searchInput.value = "";
+}
+
+getSpotifyAccessToken(clientId, clientSecret);
+
+/**** AUTORIZACIÓN PLAYLIST ****/
+const URL = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://127.0.0.1:5500/playList.html";
+const scopes =
+  "playlist-modify-private user-library-modify playlist-modify-public";
+
+function autoritzar() {
+  const authUrl =
+    URL +
+    `?client_id=${clientId}` +
+    `&response_type=token` +
+    `&redirect_uri=${redirectUri}` +
+    `&scope=${scopes}`;
+  window.location.assign(authUrl);
+}
